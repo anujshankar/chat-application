e6fdd6fe743357d63fa641359bfe357a85f559f3
Anuj Shankar <anujshankar95@gmail.com>
2016-09-10 00:04:22 +0530

    Introducing Redis

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # chat-application
 
-Created a simple chatting application using Node.JS, Express.JS and Socket.IO.
+Created a simple chatting application using Node.JS, Express.JS, Socket.IO and Redis.
--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
- 
+var redis = require('redis');
+var redisClient = redis.createClient(); //creates a new client
+
 // Initialize appication with route / (that means root of the application)
 app.get('/', function(req, res){
   var express=require('express');
@@ -13,17 +15,22 @@ app.get('/', function(req, res){
  
 var messages = [];
 function storeMessage(from,msg){
-	messages.push({name:from, data: msg});
-	if(messages.length > 10)
-		messages.shift();
+	var message = JSON.stringify({name:from, data: msg});
+	redisClient.lpush("messages",message,function(err,response){
+		redisClient.ltrim("messages",0,9); // keeps newest 10 items
+	});
 }
 
 // Register events on socket connection
 io.on('connection', function(socket){ 
   socket.on('newConnect', function(user){  
     console.log("New Connection is made!");
-	messages.forEach(function(message){
-		io.emit('join',message.name,message.data);
+    redisClient.lrange("messages",0,-1,function(err,messages){
+		messages = messages.reverse(); // so that they are emitted in correct order
+		messages.forEach(function(message){
+			message = JSON.parse(message);
+			io.emit('join',message.name,message.data);
+		});
 	});
 	io.emit('chatMessage', 'System', '<b>' + user + '</b> has joined the discussion');
   });
